b3f5e359e301006616b10fb458fa1eb1e62b0218
Siddhant Sharma <kr120sid@gmail.com>
2024-09-10 08:14:00 +0530

    Add files via upload

---
+++ NodeSnake/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Board from './Board/Board'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='App'>
+    <Board>
+
+    </Board>
+   </div>
+  )
+}
+
+export default App
